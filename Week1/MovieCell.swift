5888baf4fbd7458e3e1e4c1a2c2c30ae50a3e62c
Thuan Nguyen <thuannguyen@Thuans-MacBook-Pro.local>
2017-02-02 11:32:22 -0800

    First Commit

---
+++ Week1/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Week1
+//
+//  Created by Thuan Nguyen on 1/31/17.
+//  Copyright © 2017 Thuan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
